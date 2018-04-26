782030a0fabf34564a9522eaad6bc0cf45be5071
Oláh Péter Ciprián <peterciprian@gmail.com>
2018-04-26 16:11:15 +0200

    first commit

---
+++ routes/api.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = '../db';
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send('Api is live.');
+});
+
+
+const getDBPath = (table) => {
+    return path.join(__dirname, dbPath, `${table}.json`);
+}
+
+/**
+ * generate _ID
+ */
+const genID = (length = 25) => {
+    let chars = 'mbvcxylkjhgfdsapoiuztrewq0123456789qwertzuiopasdfghjklyxcvbnm';
+    let id = '';
+    for (let i = 0; i < length; i++) {
+        id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return id;
+}
+
+
+/**
+ *  GET all object fromt ha json file. 
+ */
+router.get('/:table', function (req, res, next) {
+    fs.readFile(getDBPath(req.params.table), 'utf8', (err, jsonData) => {
+        if (err) {
+            return res.sendStatus(404);
+        }
+        res.send(jsonData);
+    });
+
+});
+
+/**
+ * GET specified object from the json file.
+ */
+router.get('/:table/:id', function (req, res, next) {
+    fs.readFile(getDBPath(req.params.table), 'utf8', (err, jsonData) => {
+        if (err) {
+            return res.sendStatus(404);
+        }
+        //TODO: find entity by _id.
+        let entity = JSON.parse(jsonData).find(obj => obj._id === req.params.id);
+        res.send(entity);
+    });
+});
+
+/**
+ * CREATE object in a json array.
+ */
+router.post('/:table', function (req, res, next) {
+    const filePath = getDBPath(req.params.table);
+    fs.readFile(filePath, 'utf8', (err, jsonData) => {
+        if (err) {
+            return res.sendStatus(404);
+        }
+        jsonData = JSON.parse(jsonData);
+        req.body._id = genID();
+        jsonData.push(req.body);
+
+        fs.writeFileSync(filePath, JSON.stringify(jsonData), 'utf8')
+        res.json(req.body);
+    });
+});
+
+/**
+ * DELETE object by id from json array
+ */
+router.delete('/:table/:id', function (req, res, next) {
+    const filePath = getDBPath(req.params.table);
+    fs.readFile(getDBPath(req.params.table), 'utf8', (err, jsonData) => {
+        if (err) {
+            return res.sendStatus(404);
+        }
+        filteredData = JSON.parse(jsonData).filter(obj => obj._id != req.params.id);
+        fs.writeFileSync(filePath, JSON.stringify(filteredData), 'utf8')
+        res.json(`Entity deleted.`);
+    });
+});
+
+module.exports = router;
